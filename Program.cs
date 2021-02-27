2a6ac6b5f2a0187f32586908e9c527b11d8a573a
carlos eduardo <carloseduardo_alencar@outlook.com>
2021-02-26 22:46:52 -0300

    finalizando o programa Cidades

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do Litoral Sul");
+            Console.WriteLine("-----------------------------");
+            Console.ForegroundColor= ConsoleColor.DarkCyan;
+            Console.WriteLine("Peruibe, \"no rio dos tubarões\"");
+             Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Itanhaém, \"Pedra que canta\"");
+             Console.ForegroundColor= ConsoleColor.Red;
+            Console.WriteLine("Mongaguá, \"Agua pegajosa\"");
+            Console.ResetColor();
+        }
+    }
+}
